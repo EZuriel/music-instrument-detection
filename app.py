b33e7bb9b6a0bba364f692f17b0a449c772c010b
EZuriel <107522359+EZuriel@users.noreply.github.com>
2023-08-23 15:15:02 +0000

    Added 'About' page.

--- app.py
+++ app.py
@@ -7,15 +7,20 @@ app = Flask(__name__)
 def hello_world():
   return render_template('main.html')
 
-@app.route('/upload_static_file', methods=['POST'])
-def upload_static_file():
-  print("Got request in static files")
-  print(request.files)
-  f = request.files['static_file']
-  f.save(f.filename)
-  resp = {"success": True, "response": "file saved!"}
-  return jsonify(resp), 200
 
+@app.route('/about_TA')
+def about():
+  return render_template('about.html')
+
+
+# @app.route('/upload_static_file', methods=['POST'])
+# def upload_static_file():
+#   print("Got request in static files")
+#   print(request.files)
+#   f = request.files['static_file']
+#   f.save(f.filename)
+#   resp = {"success": True, "response": "file saved!"}
+#   return jsonify(resp), 200
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True)
